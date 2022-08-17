508b7699dd610589774de809422bf8f8c8a088f8
Bermet <bermet.bainaeva.ch@gmail.com>
2022-08-17 22:37:29 +0600

    next step is setup Retrofit

---
+++ app/src/main/java/com/example/movieapp/hilt/HiltModules.kt
@@ -0,0 +1,14 @@
+package com.example.movieapp.hilt
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+
+@InstallIn(SingletonComponent::class)
+@Module
+object HiltModules {
+
+
+
+}
---
+++ app/src/main/java/com/example/movieapp/ui/detail/BaseClass.kt
@@ -0,0 +1,8 @@
+package com.example.movieapp.ui.detail
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class BaseClass: Application() {
+}
