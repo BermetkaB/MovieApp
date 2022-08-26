9d9a80f1bd43b312034a9a8aecc3b3b8776f3fec
Bermet <bermet.bainaeva.ch@gmail.com>
2022-08-27 00:01:07 +0600

    binding

--- app/src/main/java/com/example/movieapp/data/Rating.kt
+++ app/src/main/java/com/example/movieapp/data/Rating.kt
@@ -1,6 +1,8 @@
 package com.example.movieapp.data
 
 data class Rating(
+    val Title: String,
+    val Year: Int,
     val Source: String,
     val Value: String
 )
--- app/src/main/java/com/example/movieapp/ui/movie/MovieFragment.kt
+++ app/src/main/java/com/example/movieapp/ui/movie/MovieFragment.kt
@@ -4,12 +4,15 @@ import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
+import android.view.View.inflate
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil.inflate
 import com.example.movieapp.R
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MovieFragment : Fragment() {
+    lateinit var binding:MovieFragmentBinding
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,8 +25,13 @@ class MovieFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
+        //need init.this line of code
+       // binding=MovieFragmentBinding.inflate(inflater, container, false)
+        // return binding.root
         return inflater.inflate(R.layout.fragment_movie, container, false)
     }
 
 
 }
+
+annotation class MovieFragmentBinding
